1fe3f34fc5e2c29083e6daa8f9be76d300ee8f1c
Jeffrey Vaughn <jvaughn1230@gmail.com>
2023-01-02 18:56:43 -0800

    signup and login pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
 
 function App() {
+  console.log(window.innerWidth);
+
   return (
     <div className="app">
       <Signup />
+      {/* <Login /> */}
     </div>
   );
 }
