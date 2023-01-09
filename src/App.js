93849366d2eb3ef9b7d792b8131184efbab2c5e5
Jeffrey Vaughn <jvaughn1230@gmail.com>
2023-01-08 17:39:02 -0800

    add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   console.log(window.innerWidth);
 
   return (
     <div className="app">
-      <Signup />
+      {/* <Signup /> */}
       {/* <Login /> */}
+      <Navbar />
     </div>
   );
 }
