ff181a1f85af0223e2e2e3f9f8ec70985799904b
Alex <jvaughn1230@gmail.com>
2023-01-27 12:20:00 -0800

    adding authSlice & connecting to login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.styles.css";
+
+import { useDispatch } from "react-redux";
+import { signIn } from "../../redux/authSlice";
 
 import logo from "../../assets/riptide-white.png";
 
 const Login = () => {
+  // TODO: remove console.log once finished
+  console.log("Login Component Loaded");
+
+  const dispatch = useDispatch();
+
+  const [input, setInput] = useState({
+    userName: "",
+    password: "",
+  });
+
+  function inputChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function submit(e) {
+    dispatch(signIn(input));
+    e.preventDefault();
+  }
+
   return (
     <div className="login-page">
       <img className="login-logo" src={logo} alt="riptide logo" />
       <div className="login-container">
         <h1>Log In</h1>
         <form className="login-form">
-          <input type="text" placeholder="Email..." className="login-input" />
+          <input
+            type="text"
+            name="userName"
+            placeholder="Username..."
+            className="login-input"
+            value={input.userName}
+            onChange={inputChange}
+          />
           <input
             type="password"
+            name="password"
             placeholder="Password..."
             className="login-input"
+            value={input.password}
+            onChange={inputChange}
           />
-          <button className="login-btn">Login</button>
+          <button className="login-btn" type="submit" onClick={submit}>
+            Login
+          </button>
         </form>
         <p className="signup-link">Need an account? Signup Here</p>
       </div>
